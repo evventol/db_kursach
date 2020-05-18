e7cfc2cd4c9921b0dbb6ed298d25491e4721d3e9
Carbon Eternity <pa03622789@gmail.com>
2020-05-18 19:06:39 +0300

    set front engine - hbs

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const exphbs = require('express-handlebars');
+
 const find_router = require('./routes/find_router');
 const insert_router = require('./routes/insert_data');
+const index_router = require('./routes/index_router');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(insert_router);
 app.use(find_router);
+app.use(index_router);
 
 
 async function start() {
---
+++ routes/index_router.js
@@ -0,0 +1,10 @@
+const {Router} = require('express');
+const index_router = Router();
+
+index_router.get('/', (req, res) => {
+    res.render('index',{
+        title: 'Index'
+    })
+});
+
+module.exports = index_router;
