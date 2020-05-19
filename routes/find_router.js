b3b959124ec4448390d99f1644df051ed26156ff
Carbon Eternity <pa03622789@gmail.com>
2020-05-19 10:40:40 +0300

    fix show workers

--- routes/find_router.js
+++ routes/find_router.js
@@ -3,17 +3,23 @@ const find_router = Router();
 
 const Employee = require('../models/Employee');
 
-find_router.get('/getall', (req, res) => {
+find_router.get('/workers', (req, res) => {
     const stream =  Employee.find().stream();
+    const emps = [];     //TODO how long?
 
     stream.on('data', function (doc) {
-        console.log(doc);
+        emps.push(doc);   //TODO need to check length, if it's OK then push
     });
     stream.on('error', function (err) {
         console.log(err);
     });
+
     stream.on('end', function () {
         console.log('All done!');
+        res.render('workers',{
+            title: 'Workers',
+            emps:emps
+        });
     });
 });
 
--- routes/index_router.js
+++ routes/index_router.js
@@ -5,36 +5,35 @@ const Employee = require('../models/Employee');
 
 
 index_router.get('/', async (req, res) => {
-    res.render('index',{
+    res.render('index', {
         title: 'Index'
     })
 });
 
-index_router.get('/workers', (req, res) => {
+/*index_router.get('/workers', (req, res) => {
     const emp =  Employee.find({},(err, emps) => {
         res.render('workers',{
             title: 'Workers',
-            emp:emps
+            emps:emps
         });
     });
+    console.log(emps);
+});*/
 
 
-
-    console.log(emp);
-});
-    // router.post('/', async (req, res) => {
-    //     const emp = new Emp({
-    //       fio: req.body.fio,
-    //       id_card: req.body.id_card,
-    //       address: req.body.address,
-    //       position: req.body.position,
-    //       salary_per_hour: req.body.salary_per_hour,
-    //       birthdate: req.body.birthdate
-    //     })
+/*    router.post('/', async (req, res) => {
+        const emp = new Emp({
+          fio: req.body.fio,
+          id_card: req.body.id_card,
+          address: req.body.address,
+          position: req.body.position,
+          salary_per_hour: req.body.salary_per_hour,
+          birthdate: req.body.birthdate
+        })
       
-    //     await emp.save()
-    //     res.redirect('/workers')
-    //   });
+        await emp.save()
+        res.redirect('/workers')
+      });*/
 
 
 module.exports = index_router;
