9da75fb7b780b5bdad99f25a6889bf33dcc71ca6
Yevhen Tolstoluzkyi <evventol@gmail.com>
2020-05-19 20:26:47 +0300

    ftp works

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ app.set('views', 'views');
 app.use("/static", express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(insert_router);
 app.use(find_router);
 app.use(index_router);
@@ -44,3 +43,21 @@ async function start() {
 }
 
 start();
+
+var Client = require('ftp');
+  var fs = require('fs');
+
+  var c = new Client();
+  c.on('ready', function() {
+    c.get('/rfidtest.zzz.com.ua/Registrate.txt', function(err, stream) {
+      if (err) throw err;
+      console.log("ftp runs")
+      stream.once('close', function() { c.end(); });
+      stream.pipe(fs.createWriteStream('tabel.txt'));
+    });
+  });
+  c.connect({
+      host:"zzz.com.ua",
+      user: "kurgan-anastasiia",
+      password:"123456789Nn"
+  });
