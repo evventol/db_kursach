b8caa03bd96c4d987315ef6060d28e9037cc4239
Carbon Eternity <pa03622789@gmail.com>
2020-05-19 11:23:29 +0300

    show 20 workers alphabetically at start

--- routes/find_router.js
+++ routes/find_router.js
@@ -5,37 +5,30 @@ const Employee = require('../models/Employee');
 const Position = require('../models/Position');
 
 find_router.get('/workers', (req, res) => {
-    const stream =  Employee.find().stream();
-    const emps = [];     //TODO how long?
+    const sort = {fio: 1};
+    const stream = Employee.find().sort(sort).stream();
+    const emps = [];
+    const maxLength = 20;
 
     stream.on('data', function (doc) {
-   /*     const newEmp = new Employee({    //TODO dont work (error)
-            fio: doc.fio,
-            id_card: doc.id_card,
-            address: doc.address,
-            position: Position.findOne({_id:doc.position}).position_name,
-            salary_per_hour: doc.salary_per_hour,
-            birthDay: doc.birthDay
-    });*/
-        const pos = Position.findById(doc.position);
-        console.log(pos.position_name);
+        if (emps.length < maxLength) {
+            emps.push(doc);
+        }
 
-
-        emps.push(doc);   //TODO need to check length, if it's OK then push
     });
+
     stream.on('error', function (err) {
         console.log(err);
     });
 
     stream.on('end', function () {
         console.log('All done!');
-        res.render('workers',{
+        res.render('workers', {
             title: 'Workers',
-            emps:emps
+            emps: emps
         });
     });
 });
-
 
 
 module.exports = find_router;
