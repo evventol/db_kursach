80b7c1bc95afa4e32541d2de3c183a203ce01c66
Carbon Eternity <pa03622789@gmail.com>
2020-05-19 10:59:45 +0300

    try to get position_name throws id

--- routes/find_router.js
+++ routes/find_router.js
@@ -2,12 +2,25 @@ const {Router} = require('express');
 const find_router = Router();
 
 const Employee = require('../models/Employee');
+const Position = require('../models/Position');
 
 find_router.get('/workers', (req, res) => {
     const stream =  Employee.find().stream();
     const emps = [];     //TODO how long?
 
     stream.on('data', function (doc) {
+   /*     const newEmp = new Employee({    //TODO dont work (error)
+            fio: doc.fio,
+            id_card: doc.id_card,
+            address: doc.address,
+            position: Position.findOne({_id:doc.position}).position_name,
+            salary_per_hour: doc.salary_per_hour,
+            birthDay: doc.birthDay
+    });*/
+        const pos = Position.findById(doc.position);
+        console.log(pos.position_name);
+
+
         emps.push(doc);   //TODO need to check length, if it's OK then push
     });
     stream.on('error', function (err) {
